4abcfe6c5f3279241d4998cd624bcb9033097d2c
AadiLASA <115965167+AadiLASA@users.noreply.github.com>
2022-12-01 06:00:51 +0400

    Updated ComplexStudent1.cpp
    Added getReal, getIm, Addition, and Subtraction Methods. Did more work on constructor. I put the class declaration stuff inside the cpp file because It's not reading complex.h yet.

--- lib/ComplexStudent1.cpp
+++ lib/ComplexStudent1.cpp
@@ -10,32 +10,61 @@ Modified : Dec. 11th
 */
 
 #include <cmath>
-#include "Complex.h"
+// #include "Complex.h"     <-------- Not working rn
 
 #define PI 3.1415926538979323846
 #define EULER 2.71828182845904523536
-
+class Complex{
+private:
+  double re = 0;
+  double im = 0;
+Complex();
+Complex(double real, double imag);
+double getReal();
+double getIm();
+Complex operator +(const Complex &num);
+Complex operator -(const Complex &num);
+};
 Complex::Complex() 
 {
   // Constructor code
-	private:
+
 		re = 0;
 		im = 0;
 }
 Complex::Complex(double real, double imag) 
 {
   // Constructor code
-	private:
 		re = real;
 		im = imag;
 }
 
+ double Complex::getReal(){
+  return re;
+}
+
+double Complex::getIm(){
+  return im;
+}
 
 
 // - - - - - Other Functions Follow - - - - - - - - - - - -
-Complex operator +(const Complex &num){
-    double real1 = num.getReal() + re; // Add original and new real portion
-  double imag1 = num.getIm() + im; // Add imaginary and new imaginary portion
-  Complex finalcomplex = new Complex((real1),(imag1)); // Merge into complex number
-  return finalcomplex; //Return complex number
+
+//Addition Operator Overload
+
+Complex Complex::operator +(const Complex &num){
+    double real1 = num.re + re; // Add original and new real portion
+  double imag1 = num.im + im; // Add imaginary and new imaginary portion
+  Complex *finalcomplex = new Complex((real1),(imag1)); // Merge into complex number
+  return *finalcomplex; //Return complex number
+}
+
+
+//Subtraction Operator Overload
+
+Complex Complex::operator -(const Complex &num){
+    double real1 = num.re - re; // Subtract original and new real portion
+  double imag1 = num.im - im; // Subtract imaginary and new imaginary portion
+  Complex *finalcomplex = new Complex((real1),(imag1)); // Merge into complex number
+  return *finalcomplex; //Return complex number
 }
