8b35a67f3fe0d2e4906e60b95c6bead59df506c0
AadiLASA <115965167+AadiLASA@users.noreply.github.com>
2022-12-01 11:00:24 +0400

    Update ComplexStuden1.cpp
    Fixed imag, and real.
    Added Magnitude and Angle.

--- lib/ComplexStudent1.cpp
+++ lib/ComplexStudent1.cpp
@@ -20,8 +20,10 @@ private:
   double im = 0;
 Complex();
 Complex(double real, double imag);
-double getReal();
-double getIm();
+double imag(const Complex&);
+double real(const Complex&);
+double abs(const Complex);
+double arg(const Complex);
 Complex operator +(const Complex &num);
 Complex operator -(const Complex &num);
 };
@@ -39,22 +41,28 @@ Complex::Complex(double real, double imag)
 		im = imag;
 }
 
- double Complex::getReal(){
-  return re;
-}
 
-double Complex::getIm(){
-  return im;
-}
 
 
 // - - - - - Other Functions Follow - - - - - - - - - - - -
 
+//Imaginary Part of Complex Num
+
+ double Complex::imag(const Complex& x){
+  return x.im;
+}
+
+//Real Part of Complex Num
+
+double Complex::real(const Complex& x){
+  return x.re;
+}
+
 //Addition Operator Overload
 
 Complex Complex::operator +(const Complex &num){
-    double real1 = num.re + re; // Add original and new real portion
-  double imag1 = num.im + im; // Add imaginary and new imaginary portion
+    double real1 = real(num) + re; // Add original and new real portion
+  double imag1 = real(num) + im; // Add imaginary and new imaginary portion
   Complex *finalcomplex = new Complex((real1),(imag1)); // Merge into complex number
   return *finalcomplex; //Return complex number
 }
@@ -63,8 +71,26 @@ Complex Complex::operator +(const Complex &num){
 //Subtraction Operator Overload
 
 Complex Complex::operator -(const Complex &num){
-    double real1 = num.re - re; // Subtract original and new real portion
-  double imag1 = num.im - im; // Subtract imaginary and new imaginary portion
+    double real1 = real(num) - re; // Subtract original and new real portion
+  double imag1 = real(num) - im; // Subtract imaginary and new imaginary portion
   Complex *finalcomplex = new Complex((real1),(imag1)); // Merge into complex number
   return *finalcomplex; //Return complex number
 }
+
+//Magnitude Function
+
+double Complex::abs(const Complex num){
+ double z =  sqrt((real(num))*(real(num)) + (real(num))*(real(num))); // calculate magnitude
+  if(z<0){
+    z=z*-1;
+    // bootleg absolute value function (magnitude has to be positive)
+  }
+  return z; //Return Magnitude
+}
+
+// Angle Function
+
+double Complex::arg(const Complex num){
+  return atan((real(num)/real(num)));
+}
+
