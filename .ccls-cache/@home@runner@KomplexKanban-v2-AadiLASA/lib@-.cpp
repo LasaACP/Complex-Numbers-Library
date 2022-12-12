5f187304906325e208bae315584eb71ff1f963ee
AadiLASA <115965167+AadiLASA@users.noreply.github.com>
2022-12-12 14:40:42 +0000

    tests r working :)

--- .ccls-cache/@home@runner@KomplexKanban-v2-AadiLASA/lib@-.cpp
+++ .ccls-cache/@home@runner@KomplexKanban-v2-AadiLASA/lib@-.cpp
@@ -2,3 +2,10 @@
 #include "../include/Complex.h"     
 #include <ostream>
 using namespace std;
+
+Complex Complex::operator -(const Complex &num){
+    double real1 = re - real(num); // Subtract original and new real portion
+  double imag1 = im - imag(num); // Subtract imaginary and new imaginary portion
+  Complex *finalcomplex = new Complex((real1),(imag1)); // Merge into complex number
+  return *finalcomplex; //Return complex number
+}
